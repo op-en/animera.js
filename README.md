1c3d67b6a057137441249726ada77cb8bd5cd95a
Anton Gustafsson <anton.gustafsson@tii.se>
2016-04-21 11:00:29 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# animera.js
-This is a javascript library that will make it easier to connect data from MQTT to visualisations on a webpage.
+# visa.js
+This is a javascript framework for visualising realtime data from MQTT via an socket.io bridge.
+
+Usage:
+
+      <!DOCTYPE html>
+      <html>
+      <head>
+      
+      <script src="socket.io-1.0.0.js"></script>	
+      <script src="visa.js"></script>
+      
+      </head>
+      <body>	
+      
+      <div mqtt='bind_topic_to_html("test/topic1",null," This could be the unit")'>This value will be replaced with data sent to test/topic1 on the mqtt broaker at op-en.se</div>
+      
+      
+      <script>
+      
+      //Create an connection to the appserver. To create your own enviroment install the playground on you own server and change the url accordingly. 
+      appserver = new AppClient("http://op-en.se:5000");
+      
+      //Bind all elements with an mqtt property to an mqtt topic.
+      appserver.autobind();
+      
+      </script>
+      
+      </body>
---
+++ widgetutils.js
@@ -0,0 +1,110 @@
+
+
+
+//Create a namespace
+var widgetutils = {};
+
+
+widgetutils.parseparams = function (paramArray = {"server":"http://op-en.se:5000","topic":"test/topic1","subproperty":null,"max":10000}) {
+
+	var href = document.defaultView.location.href;
+
+  	//Get the url of myself and parse it for parameters. 
+	if ( -1 != href.indexOf("?") )
+	{
+	var paramList = href.split("?")[1].split(/&|;/);
+	for ( var p = 0, pLen = paramList.length; pLen > p; p++ )
+	{
+	   var eachParam = paramList[ p ];
+	   var valList = eachParam.split("=");
+	   var name = unescape(valList[0]);
+	   var value = unescape(valList[1]);
+
+	   paramArray[ name ] = value.replace("\"","").replace("\"","");
+	 }
+	}
+
+	return paramArray;
+}
+
+widgetutils.getdatahub = function (name = "datahub",resource_path = ".",callback = init_widget) {
+
+	//Find top most window. 
+	var topmost = window; 
+
+	while (topmost != topmost.parent) {
+		topmost = topmost.parent
+	}
+
+	//Check if we have a data library.
+	if (!topmost.parent.hasOwnProperty(name)) {
+
+		//Dynabically load libraries. 
+		widgetutils.loadjsfile(resource_path + "/socket.io-1.0.0.js",function () { 
+			console.log("socket.io loaded")
+
+			widgetutils.loadjsfile(resource_path + "/animera.js", function () { 
+				console.log("datahub loaded")
+				callback(topmost[name]); 
+			});
+		});
+
+		return;
+
+	}
+
+	//Call callback 
+	callback(topmost[name]);
+
+}
+
+
+
+widgetutils.loadjsfile = function (filename, callback = function () {} ) {
+	
+	var script = document.createElement('script');
+
+	//console.log(typeof script);
+	
+	// most browsers
+	script.onload = callback;
+	// IE 6 & 7
+	script.onreadystatechange = function() {
+		if (this.readyState == 'complete') {
+			callback();
+		}
+	}
+	
+	if (typeof script !== "undefined") {
+        var dest = document.getElementsByTagName("head")[0];
+        //console.log(dest);
+
+        //HTML or pure SVG?
+        if (typeof dest !== "undefined") {
+        	script.type = 'text/javascript';
+			script.src = filename;
+        }
+        else {
+        	var dest = document.getElementsByTagName("svg")[0];
+        	script["xlink:href"] = filename;
+        	script["xlink:actuate"] = "onload";
+        }
+
+       //console.log(dest);
+       //console.log(script);
+       //console.log(dest);
+
+       dest.appendChild(script);
+    }
+}
+
+widgetutils.init = function () {
+
+	widgetutils.paramArray = widgetutils.parseparams();
+	widgetutils.datahub = widgetutils.getdatahub();
+
+}
+
+
+
+
